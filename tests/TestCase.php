80c7694d9d7c86f6943578e62d0685fcbcaf657f
Patrick Thomas <github@patrick-thomas.de>
2018-01-21 23:02:14 +0100

    Added missings tests (one is failing!)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,23 @@
 
 namespace Tests;
 
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, RefreshDatabase;
+
+    protected function aUser()
+    {
+        return \factory(User::class)->create();
+    }
+
+    protected function anAuthenticatedUser()
+    {
+        return tap($this->aUser(), function ($user) {
+            $this->actingAs($user);
+        });
+    }
 }
